6fd84780160f47de093790fe9752d8195d756e52
Anthony Fusco <anthony.fusco@st.com>
2018-06-05 14:00:00 +0200

    more refactor

--- StormEngine/src/main/scala/com/pastorm/encounter/Monster.scala
+++ StormEngine/src/main/scala/com/pastorm/encounter/Monster.scala
@@ -1,11 +1,11 @@
 package com.pastorm.encounter
 
-import com.pastorm.utils.ExceptionSupplierFactory.IllegalArgumentSupplier
-
 import com.ddmodel.Block
 import com.ddmodel.stat.StatType
+import com.pastorm.utils.ExceptionSupplierFactory.IllegalArgumentSupplier
 
-case class Monster(block: Block, name: String,
+case class Monster(block: Block,
+                   name: String,
                    hitPoints: Option[Int] = None,
                    initiative: Option[Int] = None) {
 
@@ -20,6 +20,5 @@ object Monster {
       hitPoints = Option(block.getStat(StatType.HIT_POINTS)
         .orElseThrow(IllegalArgumentSupplier(s"Missing HP for ${block.getName}"))
         .getStatValue.instanciateValue))
-
 
 }
