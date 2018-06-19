6531b144905c23f5d872fe37539a30b3a6423441
AnthonyFusco <anthony.fusco42@gmail.com>
2018-06-19 22:54:35 +0200

    highlighted storm editor

--- StormApi/src/main/resources/static/ace/mode-storm.js
+++ StormApi/src/main/resources/static/ace/mode-storm.js
@@ -1,45 +1,149 @@
-ace.define('ace/mode/my-mode',["require","exports","module","ace/lib/oop","ace/mode/text","ace/mode/text_highlight_rules", "ace/worker/worker_client" ], function(require, exports, module) {
-    var oop = require("ace/lib/oop");
-    var TextMode = require("ace/mode/text").Mode;
-    var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+define("ace/mode/storm_highlight_rules", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text_highlight_rules"],
+    function (require, exports, module) {
+        "use strict";
 
-    var MyHighlightRules = function() {
-        var keywordMapper = this.createKeywordMapper({
-            "keyword.control": "if|then|else",
-            "keyword.operator": "and|or|not",
-            "keyword.other": "class",
-            "storage.type": "int|float|text",
-            "storage.modifier": "private|public",
-            "support.function": "print|sort",
-            "constant.language": "true|false"
-        }, "identifier");
-        this.$rules = {
-            "start": [
-                { token : "comment", regex : "//" },
-                { token : "string",  regex : '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]' },
-                { token : "constant.numeric", regex : "0[xX][0-9a-fA-F]+\\b" },
-                { token : "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b" },
-                { token : "keyword.operator", regex : "!|%|\\\\|/|\\*|\\-|\\+|~=|==|<>|!=|<=|>=|=|<|>|&&|\\|\\|" },
-                { token : "punctuation.operator", regex : "\\?|\\:|\\,|\\;|\\." },
-                { token : "paren.lparen", regex : "[[({]" },
-                { token : "paren.rparen", regex : "[\\])}]" },
-                { token : "text", regex : "\\s+" },
-                { token: keywordMapper, regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b" }
-            ]
+        var oop = require("../lib/oop");
+        var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+
+        var StormHighlightRules = function () {
+            this.$rules = {
+                "start": [
+                    {
+                        token: "keyword",
+                        regex: /^[^0-9].+\n*/,
+                        next: "stats"
+                    }
+                ],
+                "stats": [
+                    {
+                        token: "entity.name.function",
+                        regex: /ac |AC |pp |PP |hp |HP |speed |SPEED |str |dex |con |int |wis |cha /
+                    }, {
+                        token: "constant.numeric",
+                        regex: /-|[+|-]|\d+d\d+|\d+/
+                    }, {
+                        token: "constant.numeric",
+                        regex: /\d/
+                    }, {
+                        token: "keyword",
+                        regex: /^[^0-9].+\n*/,
+                        next: "ability"
+                    }
+                ],
+                "ability": [
+                    {
+                        token: "entity.name.function",
+                        regex: /ac |AC |pp |PP |hp |HP |speed |SPEED |str |dex |con |int |wis |cha /,
+                        next: "stats"
+                    }, {
+                        token: "constant.numeric",
+                        regex: /[+|-]\s*\d+/
+                    }, {
+                        token: "entity.name.function",
+                        regex: /to hit/,
+                        next: "abilityBlock"
+                    }
+                ],
+                "abilityBlock": [
+                    {
+                        token: "entity.name.function",
+                        regex: /hit|reach|range/
+                    }, {
+                        token: "constant.numeric",
+                        regex: /\d|d|[+|-]/
+                    }, {
+                        token: "entity.name.function",
+                        regex: /(piercing|slashing|bludgeoning|fire|acid|ice|arcane|thunder)/
+                    }, {
+                        token: "paren",
+                        regex: /{/,
+                        next: "description"
+                    }, {
+                        token: "keyword",
+                        regex: /^[^0-9][^=>]+\s*\n*/,
+                        next: "featureOrAbility"
+                    }, {
+                        defaultToken: "text"
+                    }
+                ],
+                "featureOrAbility": [
+                    {
+                        token: "entity.name.function",
+                        regex: /=>/,
+                        next: "feature"
+                    }, {
+                        token: "entity.name.function",
+                        regex: /[^+{=>]+\s*.*\s*\n*/,
+                        next: "abilityBlock"
+                    }, {
+                        token: "constant.numeric",
+                        regex: /[+|-]\s*\d+/
+                    }, {
+                        token: "entity.name.function",
+                        regex: /to hit/,
+                        next: "abilityBlock"
+                    }, {
+                        token: "paren",
+                        regex: /{/,
+                        next: "description"
+                    }
+                ],
+                "description": [
+                    {
+                        token: "paren",
+                        regex: /}/,
+                        next: "abilityBlock"
+                    }, {
+                        defaultToken: "text"
+                    }
+                ],
+                "feature": [
+                    {
+                        token: "entity.name.function",
+                        regex: /=>/
+                    }, {
+                        token: "paren",
+                        regex: /{/
+                    }, {
+                        token: "paren",
+                        regex: /}/,
+                        next: "featureName"
+                    }, {
+                        defaultToken: "text"
+                    }
+                ],
+                "featureName": [
+                    {
+                        token: "entity.name.function",
+                        regex: /=>/
+                    }, {
+                        token: "paren",
+                        regex: /{/,
+                        next: "feature"
+                    }, {
+                        token: "keyword",
+                        regex: /^[^0-9][^=>{}]+\n*/
+                    }
+                ]
+            };
         };
-    };
-    oop.inherits(MyHighlightRules, TextHighlightRules);
+        oop.inherits(StormHighlightRules, TextHighlightRules);
 
-    var MyMode = function() {
-        this.HighlightRules = MyHighlightRules;
-    };
-    oop.inherits(MyMode, TextMode);
+        exports.StormHighlightRules = StormHighlightRules;
+    });
 
-    (function() {
+define("ace/mode/storm", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text", "ace/mode/storm_highlight_rules"],
+    function (require, exports, module) {
+        "use strict";
 
-        this.$id = "ace/mode/storm";
+        const oop = require("../lib/oop");
+        const TextMode = require("./text").Mode;
+        const HighlightRules = require("./storm_highlight_rules").StormHighlightRules;
 
-    }).call(MyMode.prototype);
+        const Mode = function () {
+            this.HighlightRules = HighlightRules;
+        };
+        oop.inherits(Mode, TextMode);
 
-    exports.Mode = MyMode;
-});
+        exports.Mode = Mode;
+    });
