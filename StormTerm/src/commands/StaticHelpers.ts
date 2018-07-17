125a75719775c15a67c8f5cc9852296a9f7b0ce9
AnthonyFusco <anthony.fusco@st.com>
2018-07-17 18:17:47 +0200

    test of build with electron

--- StormTerm/src/commands/StaticHelpers.ts
+++ StormTerm/src/commands/StaticHelpers.ts
@@ -15,6 +15,7 @@ import {SetInitiativeCommand} from "./SetInitiativeCommand";
 import {GetBlocksCommand} from "./GetBlocksCommand";
 import {GetPlayingMonsterCommand} from "./GetPlayingMonsterCommand";
 import {Application} from "../Application";
+import * as $ from "jquery";
 
 export class StaticHelpers {
     static hideSpinner(): void {
