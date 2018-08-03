d857f4295c2bcd80d4099ac6f13607070d24695a
AnthonyFusco <anthony.fusco@st.com>
2018-08-03 16:42:41 +0200

    working storm listener

--- StormWeb/stormLanguage/src/index.ts
+++ StormWeb/stormLanguage/src/index.ts
@@ -2,8 +2,9 @@
 
 import {ANTLRInputStream, CommonTokenStream} from 'antlr4ts';
 import {StormLexer} from '../parser/StormLexer';
-import {StormParser} from '../parser/StormParser';
+import {Action_block_nameContext, Block_nameContext, StormParser} from '../parser/StormParser';
 import {StormListener} from '../parser/StormListener';
+import {ParseTreeWalker} from "antlr4ts/tree";
 
 
 class MyListener implements StormListener {
@@ -13,36 +14,62 @@ class MyListener implements StormListener {
         this.parser = parser;
     }
 
-    enterBlock(ctx) {
-        console.log(ctx);
-        console.log('name: ' + ctx.block_name().getText());
-        console.log('ability block: ' + ctx.ability_block());
-    }
+    enterBlock_name (ctx: Block_nameContext) {
+        console.log('name: ' + ctx.text);
+    };
 
-    getBlock() {
-        console.log('totototo');
-    }
+    exitAction_block_name (ctx: Action_block_nameContext) {
+        console.log('action: ' + ctx.text);
+    };
 
 }
 
 class Startup {
     public static main(): number {
-        console.log("ewojgfwegf")
-        // Create the lexer and parser
-        let inputStream = new ANTLRInputStream("text");
+        let b =
+`
+Acolyte
+
+AC 10
+HP 2d8
+speed 30
+pp 10
+
+str 10
+dex 10
+con 10
+int 10
+wis 14
+cha 11
+
+actions {
+
+Club =>
+{Melee Weapon Attack: +2 to hit, reach 5 ft., one target. Hit: 2 (1d4) bludgeoning damage.}
+
+}
+features {
+
+Skills => {medicine 4, religion 2}
+
+Languages => {any one language (usually Common)}
+
+Challenge => {1/4}
+
+}
+`
+        let inputStream = new ANTLRInputStream(b);
         let lexer = new StormLexer(inputStream);
         let tokenStream = new CommonTokenStream(lexer);
         let parser = new StormParser(tokenStream);
         // let ctx = parser.block();
 
-        let listener = new MyListener(parser);
 
         parser.buildParseTree = true;
         let tree = parser.block();
-        // let printer = new ExtractInterfaceListener(parser);
-        // ParseTreeWalker.DEFAULT.walk(listener, tree);
+        let listener = new MyListener(parser);
+        ParseTreeWalker.DEFAULT.walk(listener, tree);
 
-        listener.getBlock();
 
         return 0;
     }
