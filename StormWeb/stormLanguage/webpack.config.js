49204afb2864162c69997afd4bc4d84e443f1c69
Florent Pastor <flopastor06@gmail.com>
2018-08-03 16:21:37 +0200

    storm parser js

--- StormWeb/stormLanguage/webpack.config.js
+++ StormWeb/stormLanguage/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
@@ -17,9 +18,28 @@ module.exports = {
             }
         ]
     },
-    node: {
-        module: "empty",
-        net:"empty",
-        fs: "empty"
+    resolve: {
+        extensions:[".ts", ".js", ".json"]
     }
 };
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map';
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
