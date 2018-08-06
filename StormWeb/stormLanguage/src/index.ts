d10a960e979169336395a24a0f0f0c6ce668efb1
earendil06 <flopastor06@gmail.com>
2018-08-06 23:03:51 +0200

    adapter js storm parser

--- StormWeb/stormLanguage/src/index.ts
+++ StormWeb/stormLanguage/src/index.ts
@@ -5,7 +5,6 @@ import {StormLexer} from '../parser/StormLexer';
 import {
     BlockContext,
     StormParser,
-    Ability_blockContext,
     Action_blockContext,
     Feature_blockContext,
     StatContext, AbilityContext
@@ -14,48 +13,90 @@ import {StormListener} from '../parser/StormListener';
 import {ParseTreeWalker} from "antlr4ts/tree";
 import {ParseTreeListener} from "antlr4ts/tree/ParseTreeListener";
 
-// import * as Engine from "./../../../engine/target/scala-2.12/engine-fastopt.js"
-
 class MyListener implements StormListener {
     private blockAdapter = new (window as any).BlockAdapter();
-    private parser : StormParser;
-    constructor(parser: StormParser){
+    private parser: StormParser;
+
+    constructor(parser: StormParser) {
         this.parser = parser;
     }
 
-    enterBlock (ctx: BlockContext) {
+    enterBlock(ctx: BlockContext) {
         const name = ctx.block_name().text.toLowerCase();
         this.blockAdapter.setName(name);
-        console.log(this.blockAdapter);
-        //todo set name adapter
     }
 
-    enterAbility (ctx: AbilityContext){
-        const type = ctx.STAT_ID().symbol;
+    enterAbility(ctx: AbilityContext) {
+        const type = ctx.STAT_ID().symbol.text.toLowerCase();
+        const value = parseInt(ctx.NUMBER().symbol.text);
+        const f = (value - 10) > 0 ? Math.floor : Math.ceil;
+        const substractValue = value < 10 ? -1 : 0;
+        const modifier = f((value - 10) / 2) + substractValue;
+        this.blockAdapter.putAbility(type, value, modifier);
     }
 
 
     enterAction_block(ctx: Action_blockContext) {
-
+        const actions = ctx.action_component();
+        let reach = "", range = "", toHit = "", hit = "", description = "";
+        actions.forEach(actionComponent => {
+            if (actionComponent.reach() != null) {
+                reach = actionComponent.reach().text;
+            }
+            if (actionComponent.range() != null) {
+                range = actionComponent.range().text;
+            }
+            if (actionComponent.to_hit() != null) {
+                toHit = actionComponent.to_hit().text;
+            }
+            if (actionComponent.hit() != null) {
+                hit = actionComponent.hit().text;
+            }
+        });
+        if (ctx.description() != null) {
+            description = ctx.description().text;
+        }
+        const action = new (window as any).Action(ctx.action_block_name().text.replace("\n", ""), toHit, reach, range, hit, description);
+        this.blockAdapter.putAction(action);
     }
-    enterFeature_block (ctx: Feature_blockContext){
+
+    enterFeature_block(ctx: Feature_blockContext) {
         const name = ctx.feature_name().text;
         const description = ctx.description().text;
-        //todo put feature
+        this.blockAdapter.putFeature(name, description.replace("[{}]", ""));
     }
-    enterStat (ctx: StatContext){}
+
+    enterStat(ctx: StatContext) {
+        let statValue = "";
+        if (ctx.NUMBER() == null) {
+            const number = parseInt(ctx.dice().NUMBER(0).text);
+            const faces = parseInt(ctx.dice().NUMBER(1).text);
+            const modifierContext = ctx.dice().modifier();
+            let modifierValue = 0;
+            if (modifierContext != null) {
+                const modifier = modifierContext.NUMBER().text;
+                modifierValue = parseInt(modifier);
+                const op = modifierContext.MODIFIER_OP().text;
+                modifierValue = "+" === op ? modifierValue : -modifierValue;
+            }
+            statValue = new (window as any).DiceValue(number, faces, modifierValue);
+        } else {
+            statValue = new (window as any).ConstValue(ctx.NUMBER().text, parseInt(ctx.NUMBER().text));
+        }
+        this.blockAdapter.putStat(ctx.STAT().symbol.text.toLowerCase(), statValue);
+    }
 
 
-
-
+    public getResult() {
+        return this.blockAdapter.block;
+    }
 
 }
 
 class Startup {
     public static main(): number {
         let b =
-`
-Acolyte
+            `Acolyte
 
 AC 10
 HP 2d8
@@ -71,10 +112,18 @@ cha 11
 
 actions {
 
-Club =>
-{Melee Weapon Attack: +2 to hit, reach 5 ft., one target. Hit: 2 (1d4) bludgeoning damage.}
+scimitar =>
++4 to hit
+reach 5
+hit 1d6+2 slashing
 
+shortbow =>
++4 to hit
+range 80/320
+hit 1d6 + 2 piercing
+{cool bow}
 }
+
 features {
 
 Skills => {medicine 4, religion 2}
@@ -84,19 +133,17 @@ Languages => {any one language (usually Common)}
 Challenge => {1/4}
 
 }
-`
+`;
         let inputStream = new ANTLRInputStream(b);
         let lexer = new StormLexer(inputStream);
         let tokenStream = new CommonTokenStream(lexer);
         let parser = new StormParser(tokenStream);
-        // let ctx = parser.block();
-
 
         parser.buildParseTree = true;
         let tree = parser.block();
         let listener = new MyListener(parser) as ParseTreeListener;
         ParseTreeWalker.DEFAULT.walk(listener, tree);
-
+        console.log((listener as MyListener).getResult());
 
         return 0;
     }
