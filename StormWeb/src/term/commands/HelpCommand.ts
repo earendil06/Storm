24c7415540953b0eea6ad6617932d607a8f79ad9
Anthony Fusco <anthony.fusco42@gmail.com>
2018-08-17 22:51:25 +0200

    better fixed position for encounter

--- StormWeb/src/term/commands/HelpCommand.ts
+++ StormWeb/src/term/commands/HelpCommand.ts
@@ -13,7 +13,9 @@ export default class HelpCommand extends Command {
 
     async execute(args: string[]): Promise<IHistoryCommand> {
         if (args.length === 0) {
-            return new HistoryCommand(this.getCommandName(), args, StaticHelpers.COMMANDS().map(c => c.getCommandName()).join(", "), "default-component");
+            let helpText = StaticHelpers.COMMANDS().map(c => c.getCommandName()).join(", ");
+            helpText += '\n\nUse "help [command]" to learn more about a specific command';
+            return new HistoryCommand(this.getCommandName(), args, helpText, "markdown-component");
         } else {
             try {
                 let markdown = await new LocalAccessor().loadFileByName("helps/" + args[0] + ".md");
