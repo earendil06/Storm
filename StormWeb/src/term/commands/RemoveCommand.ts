eba81a5da4cc77141a6227094027363fa55fb4a4
AnthonyFusco <anthony.fusco@st.com>
2018-08-21 14:24:48 +0200

    working remove command

--- StormWeb/src/term/commands/RemoveCommand.ts
+++ StormWeb/src/term/commands/RemoveCommand.ts
@@ -1,10 +1,9 @@
 import Command from "./Command";
 import {StaticHelpers} from "../../StaticHelpers";
-import * as $ from "jquery";
 import {IHistoryCommand} from "../../Application";
 import {HistoryCommand} from "../../poco/HistoryCommand";
 
-export class RemoveCommand extends Command{
+export class RemoveCommand extends Command {
 
     constructor() {
         super("remove");
@@ -16,18 +15,10 @@ export class RemoveCommand extends Command{
         } else {
             const monsterName = args[0];
             try {
-                /*const result = await $.ajax({
-                    contentType: "application/json",
-                    method: 'DELETE',
-                    url: `http://${StaticHelpers.server}:${StaticHelpers.port}/api/remove/` + monsterName
-                });*/
+                StaticHelpers.engine().remove(monsterName);
                 return new HistoryCommand(this.getCommandName(), args, monsterName + " has been removed from the encounter.", "default-component");
             } catch (e) {
-                switch (e.status) {
-                    case 404 : return new HistoryCommand(this.getCommandName(), args, monsterName + " does not exists in the encounter.", "default-component");
-                    case 500 : return new HistoryCommand(this.getCommandName(), args, "Error 500, Something went wrong!", "default-component");
-                    default : return null;
-                }
+                return new HistoryCommand(this.getCommandName(), args, monsterName + " does not exists in the encounter.", "default-component");
             }
         }
     }
