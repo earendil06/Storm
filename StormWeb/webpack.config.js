59f28ada4e368db091d75e777033c6faa0dab105
earendil06 <flopastor06@gmail.com>
2018-09-04 21:53:20 +0200

    releases

--- StormWeb/webpack.config.js
+++ StormWeb/webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
     mode: 'development',
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        path: path.resolve(__dirname, './generated'),
+        publicPath: '/generated/',
         filename: 'build.js'
     },
 
