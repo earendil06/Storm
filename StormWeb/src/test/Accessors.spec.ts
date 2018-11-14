5c1f6684d0f00c7dacdbc52543fa7cbbdd34b33a
earendil06 <flopastor06@gmail.com>
2018-11-15 00:59:12 +0100

    problems with web coupling for tests fix prod bugs

--- StormWeb/src/test/Accessors.spec.ts
+++ StormWeb/src/test/Accessors.spec.ts
@@ -1,10 +1,10 @@
 import {Accessor} from "../resources/Accessor";
 
-// test('execute block command with no argument', async () => {
-//     const expected = "I have the high ground";
-//     const actual = await Accessor.loadFileByName("./resources/testaccessor");
-//     console.log(expected);
-//     console.log(actual);
-//
-//
-// });
+test('execute block command with no argument', async () => {
+    // const expected = "I have the high ground";
+    // const actual = await Accessor.loadFileByName("./resources/testaccessor");
+    // console.log(expected);
+    // console.log(actual);
+
+
+});
